72fb7f62e5618d36eb8609de5109a783f125233c
lewiswolf <lewiswolstanholme@gmail.com>
2020-08-13 22:47:52 +0100

    cleaning

--- example/src/App.js
+++ example/src/App.js
@@ -4,21 +4,22 @@ import * as MaxMSP from 'maxmsp-gui'
 import 'maxmsp-gui/dist/index.css'
 
 export default function App() {
-  return (<React.Fragment>
-    <h2>React component library for stylised Max MSP GUI</h2>
-    <MaxMSP.Bang />
-    <MaxMSP.Ezdac />
-    <MaxMSP.Message text={'text'} />
-    <MaxMSP.Object text={'text'} />
-    <MaxMSP.Playbar />
-    <MaxMSP.RadioGroup />
-    <MaxMSP.Slider />
-    <div className='textbuttons'>
-      <MaxMSP.TextButton mode={false} />
-      <MaxMSP.TextButton mode={true} />
-    </div>
-    <MaxMSP.Toggle />
-    <MaxMSP.Umenu items={['foo', 'bar']} />
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      <h2>React component library for stylised Max MSP GUI</h2>
+      <MaxMSP.Bang />
+      <MaxMSP.Ezdac />
+      <MaxMSP.Message text={'text'} />
+      <MaxMSP.Object text={'text'} />
+      <MaxMSP.Playbar />
+      <MaxMSP.RadioGroup />
+      <MaxMSP.Slider />
+      <div className='textbuttons'>
+        <MaxMSP.TextButton mode={false} />
+        <MaxMSP.TextButton mode={true} />
+      </div>
+      <MaxMSP.Toggle />
+      <MaxMSP.Umenu items={['foo', 'bar']} />
+    </React.Fragment>
   )
 }
