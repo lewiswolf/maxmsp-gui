b0115f27b36782fb3f0f17fc09a6426dec16b029
lewiswolf <lewiswolstanholme@gmail.com>
2020-10-02 19:48:24 +0100

    radiogroup now has touch and screen reader support

--- src/components/radiogroup.jsx
+++ src/components/radiogroup.jsx
@@ -1,24 +1,46 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import propTypes from 'prop-types'
 import { ReactComponent as SVG } from '../svg/radiogroup-row.svg'
 import style from '../scss/radiogroup.module.scss'
 
 export default class RadioGroup extends Component {
     static propTypes = {
+        ariaLabel: propTypes.string,
         items: propTypes.array,
-        value: propTypes.number,
         spacing: propTypes.number,
+        value: propTypes.number,
         onClick: propTypes.func,
     }
 
     static defaultProps = {
+        ariaLabel: 'radiogroup',
         items: ['', ''],
-        value: 0,
         spacing: 20,
+        value: 0,
+        onClick: () => {},
     }
 
-    state = {
-        index: this.props.value,
+    constructor(props) {
+        super(props)
+        this.state = {
+            index: this.props.value,
+        }
+        this.touchstart.bind(this)
+    }
+
+    componentDidMount() {
+        ReactDOM.findDOMNode(this).addEventListener(
+            'touchstart',
+            this.touchstart
+        )
+    }
+
+    componentWillUnmount() {
+        ReactDOM.findDOMNode(this).removeEventListener(
+            'touchstart',
+            this.touchstart
+        )
     }
 
     componentDidUpdate(prevProps) {
@@ -29,6 +51,32 @@ export default class RadioGroup extends Component {
         }
     }
 
+    touchstart = (e) => {
+        if (e.cancelable) {
+            e.preventDefault()
+            let t
+            ReactDOM.findDOMNode(this).childNodes.forEach((button, i) => {
+                let b = button.getBoundingClientRect()
+                if (
+                    e.targetTouches[0].clientY > b.top &&
+                    e.targetTouches[0].clientY < b.bottom
+                ) {
+                    t = i + 1
+                }
+            })
+            this.togglePressed(t)
+        }
+    }
+
+    togglePressed = (i) => {
+        this.setState(
+            {
+                index: i !== this.state.index ? i : 0,
+            },
+            () => this.props.onClick(this.state.index)
+        )
+    }
+
     render() {
         return (
             <div className={style.radiogroup}>
@@ -37,19 +85,20 @@ export default class RadioGroup extends Component {
                     return (
                         <div
                             key={i}
-                            aria-label={`radiogroup button ${i}`}
-                            role='button'
-                            aria-pressed={i === this.state.index}
-                            onMouseDown={() => {
-                                this.setState(
-                                    {
-                                        index: i !== this.state.index ? i : 0,
-                                    },
-                                    () => {
-                                        this.props.onClick &&
-                                            this.props.onClick(this.state.index)
-                                    }
-                                )
+                            aria-label={
+                                item ? item : `${this.props.ariaLabel}: ${i}`
+                            }
+                            aria-checked={i === this.state.index}
+                            role='switch'
+                            tabIndex='0'
+                            onMouseDown={(e) =>
+                                e.button === 0 && this.togglePressed(i)
+                            }
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter' || e.key === ' ') {
+                                    e.preventDefault()
+                                    this.togglePressed(i)
+                                }
                             }}
                             style={{
                                 height:
@@ -58,26 +107,32 @@ export default class RadioGroup extends Component {
                                         : '16px',
                             }}
                         >
-                            <p
+                            {item && (
+                                <p
+                                    tabIndex='-1'
+                                    style={{
+                                        paddingRight: item ? '10px' : 0,
+                                        lineHeight:
+                                            this.props.spacing > 16
+                                                ? `${this.props.spacing}px`
+                                                : '16px',
+                                        outline: 0,
+                                    }}
+                                >
+                                    {item}
+                                </p>
+                            )}
+                            <div
+                                tabIndex='-1'
                                 style={{
-                                    paddingRight: item ? '10px' : 0,
-                                    lineHeight:
-                                        this.props.spacing > 16
-                                            ? `${this.props.spacing}px`
-                                            : '16px',
+                                    outline: 0,
+                                    background:
+                                        i === this.state.index
+                                            ? 'radial-gradient(18px circle at center,#cee5e8 50%,#333333 50%)'
+                                            : 'radial-gradient(18px circle at center,#595959 50%,#333333 50%)',
                                 }}
                             >
-                                {item}
-                            </p>
-                            <div>
-                                <SVG
-                                    style={{
-                                        background:
-                                            i === this.state.index
-                                                ? 'radial-gradient(18px circle at center,#cee5e8 50%,#333333 50%)'
-                                                : 'radial-gradient(18px circle at center,#595959 50%,#333333 50%)',
-                                    }}
-                                />
+                                <SVG />
                             </div>
                         </div>
                     )
