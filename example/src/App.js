ad5a7e9669e668d801d1f026abd0c331d6da9c54
lewiswolf <lewiswolstanholme@gmail.com>
2021-03-20 19:07:30 +0000

    prettier changed the tabs

--- example/src/App.js
+++ example/src/App.js
@@ -6,52 +6,52 @@ import 'maxmsp-gui/dist/index.css'
 window.iframeResizer = iframeResizer;
 
 export default class App extends Component {
-  state = {
-    playing: false,
-    playbar: 0,
-    iframe: window !== window.top,
-  }
+	state = {
+		playing: false,
+		playbar: 0,
+		iframe: window !== window.top,
+	}
 
-  render() {
-    return (
-      <main>
-        {!this.state.iframe && <h2>React component library for stylised Max MSP GUI</h2>}
-        <MaxMSP.Bang />
-        <MaxMSP.Ezdac />
-        <MaxMSP.Message text='text' />
-        <MaxMSP.Object text='text' />
-        <MaxMSP.Playbar
-          isPlaying={(bool) => {
-            if (bool) {
-              this.interval = setInterval(() => {
-                if (this.state.playbar + 100 / 500 <= 100) {
-                  this.setState({ playbar: this.state.playbar + 100 / 500, playing: true })
-                } else {
-                  clearInterval(this.interval)
-                  this.setState({
-                    playbar: 0,
-                    playing: false,
-                  })
-                }
-              }, 10)
-            } else {
-              clearInterval(this.interval)
-              this.setState({ playing: bool })
-            }
-          }}
-          setPlaying={this.state.playing}
-          value={this.state.playbar}
-          onChange={(i) => this.setState({ playbar: i })}
-        />
-        <MaxMSP.RadioGroup />
-        <MaxMSP.Slider />
-        <div className='textbuttons'>
-          <MaxMSP.TextButton mode={false} />
-          <MaxMSP.TextButton mode={true} />
-        </div>
-        <MaxMSP.Toggle />
-        <MaxMSP.Umenu items={['foo', 'bar']} />
-      </main>
-    )
-  }
+	render() {
+		return (
+			<main>
+				{!this.state.iframe && <h2>React component library for stylised Max MSP GUI</h2>}
+				<MaxMSP.Bang />
+				<MaxMSP.Ezdac />
+				<MaxMSP.Message text='text' />
+				<MaxMSP.Object text='text' />
+				<MaxMSP.Playbar
+					isPlaying={(bool) => {
+						if (bool) {
+							this.interval = setInterval(() => {
+								if (this.state.playbar + 100 / 500 <= 100) {
+									this.setState({ playbar: this.state.playbar + 100 / 500, playing: true })
+								} else {
+									clearInterval(this.interval)
+									this.setState({
+										playbar: 0,
+										playing: false,
+									})
+								}
+							}, 10)
+						} else {
+							clearInterval(this.interval)
+							this.setState({ playing: bool })
+						}
+					}}
+					setPlaying={this.state.playing}
+					value={this.state.playbar}
+					onChange={(i) => this.setState({ playbar: i })}
+				/>
+				<MaxMSP.RadioGroup />
+				<MaxMSP.Slider />
+				<div className='textbuttons'>
+					<MaxMSP.TextButton mode={false} />
+					<MaxMSP.TextButton mode={true} />
+				</div>
+				<MaxMSP.Toggle />
+				<MaxMSP.Umenu items={['foo', 'bar']} />
+			</main>
+		)
+	}
 }
--- example/src/index.js
+++ example/src/index.js
@@ -4,6 +4,6 @@ import App from './App'
 import './index.css'
 
 render(
-    <App />,
-    document.getElementById('root')
+	<App />,
+	document.getElementById('root')
 )
