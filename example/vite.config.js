375861757943ae78ba31f45aa5de34fe24f1af36
lewiswolf <lewiswolstanholme@gmail.com>
2023-02-10 18:55:33 +0000

    never pushed the fixes to gh-pages

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	base: 'maxmsp-gui',
+	plugins: [ghPages(), react()],
 })
