a1ba5fe8145e339ab7169e766d16290b928709e8
lewiswolf <lewiswolstanholme@gmail.com>
2024-07-13 16:49:51 +0100

    added compression to the example website

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/maxmsp-gui',
-	plugins: [react()],
+	build: { target: 'ESNext' },
+	esbuild: { legalComments: 'none' },
+	plugins: [
+		compression({
+			algorithm: 'gzip',
+			include: /\.(js|mjs|json|css|svg)$/i,
+		}),
+		react(),
+	],
 })
